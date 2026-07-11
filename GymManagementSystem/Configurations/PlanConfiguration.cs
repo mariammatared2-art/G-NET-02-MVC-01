eaf60ddd211d8490a713044e20c0a9d6fcdce1b3
mariam matared <mariammatared2@gmail.com>
2026-07-11 20:05:22 +0200

    Add project files.

---
+++ GymManagementSystem/Configurations/PlanConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymManagementSystem.Configrations
+{
+    public class PlanConfiguration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(P => P.Name)
+                .HasColumnType("Varchar")
+                .HasMaxLength(50);
+            builder.Property(P => P.Description)
+                .HasMaxLength (200);
+            builder.Property(P => P.Price)
+                .HasPrecision (10,2);
+            builder.Property(P => P.CreateAt)
+                .HasDefaultValueSql("GetDate()");
+            builder.ToTable(TB =>
+            {
+                TB.HasCheckConstraint("PlanDurationCheck", "DurationDays Between 1 and 365");
+            });
+        }
+    }
+}
---
+++ GymManagementSystem/Controllers/PlansController.cs
@@ -0,0 +1,30 @@
+﻿using GymManagementSystem.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext _dbContext;
+        public PlansController()
+        {
+            _dbContext = new GymDbContext();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Plans = await _dbContext.Plans.ToListAsync();
+            return View(Plans);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await _dbContext.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+    }
+}
