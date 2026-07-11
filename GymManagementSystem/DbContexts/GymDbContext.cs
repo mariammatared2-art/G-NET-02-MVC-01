44beee0c37ea4fd666ce0dad07334103c02647a2
mariam matared <mariammatared2@gmail.com>
2026-07-11 20:16:30 +0200

    GymDbContext

--- GymManagementSystem/DbContexts/GymDbContext.cs
+++ GymManagementSystem/DbContexts/GymDbContext.cs
@@ -4,16 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GymManagementSystem.DbContexts
 {
-    public class GymDbContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=.;Database=GymDbG02;Trusted_Connection=True;TrustServerCertificate=True");
-        }
-        public DbSet<Plan> Plans { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new PlanConfiguration());
-        }
-    }
+    #region GymDbContext
+    //public class GymDbContext : DbContext
+    //{
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlServer("Server=.;Database=GymDbG02;Trusted_Connection=True;TrustServerCertificate=True");
+    //    }
+    //    public DbSet<Plan> Plans { get; set; }
+    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.ApplyConfiguration(new PlanConfiguration());
+    //    }
+    //}
+    #endregion
 }
