c55c6ead16e12e2e27be68b4d6030773fd0425b6
mariam matared <mariammatared2@gmail.com>
2026-07-11 20:15:34 +0200

    Plans

--- GymManagementSystem/Controllers/PlansController.cs
+++ GymManagementSystem/Controllers/PlansController.cs
@@ -5,26 +5,28 @@ using System.Threading.Tasks;
 
 namespace GymManagementSystem.Controllers
 {
-    public class PlansController : Controller
-    {
-        private readonly GymDbContext _dbContext;
-        public PlansController()
-        {
-            _dbContext = new GymDbContext();
-        }
-        public async Task<IActionResult> Index()
-        {
-            var Plans = await _dbContext.Plans.ToListAsync();
-            return View(Plans);
-        }
-        public async Task<IActionResult> Details(int id)
-        {
-            var plan = await _dbContext.Plans.FindAsync(id);
-            if (plan == null)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            return View(plan);
-        }
-    }
+    #region Plans
+    //public class PlansController : Controller
+    //{
+    //    private readonly GymDbContext _dbContext;
+    //    public PlansController()
+    //    {
+    //        _dbContext = new GymDbContext();
+    //    }
+    //    public async Task<IActionResult> Index()
+    //    {
+    //        var Plans = await _dbContext.Plans.ToListAsync();
+    //        return View(Plans);
+    //    }
+    //    public async Task<IActionResult> Details(int id)
+    //    {
+    //        var plan = await _dbContext.Plans.FindAsync(id);
+    //        if (plan == null)
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        return View(plan);
+    //    }
+    //}
+    #endregion
 }
